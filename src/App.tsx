6cae1ffea8207c8162c3c8837f6b903b27a0f611
PlayerOne <playerprogramming@gmail.com>
2022-04-29 00:22:15 -0700

    Implemented UI part

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [word, setWord] = useState("");
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt"
+    )
+      .then((words) => words.text())
+      .then((textedWords) => setWord(textedWords));
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Wordle Answer</p>
       </header>
+      <body className="App-body">
+        <div>
+          <input className="App-Input" type={"text"} maxLength={1} />
+          <input className="App-Input" type={"text"} maxLength={1} />
+          <input className="App-Input" type={"text"} maxLength={1} />
+          <input className="App-Input" type={"text"} maxLength={1} />
+          <input className="App-Input" type={"text"} maxLength={1} />
+        </div>
+        <p>API calling</p>
+        <p>{word}</p>
+      </body>
     </div>
   );
 }
 
-export default App;
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
