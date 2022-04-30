ba9b17215f4e6c9b74ef045e22d3f11a9315e385
PlayerOne <playerprogramming@gmail.com>
2022-04-29 22:27:46 -0700

    Implemented small logic to fetch all the texts into an array using async&await

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,41 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-
+async function test1() {
+  let array1;
+  let arr;
+  await fetch(
+    "https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt"
+  )
+    .then((words) => words.text())
+    .then((textedWords) => {
+      array1 = textedWords.split("\n");
+      arr = textedWords.replace(/\r\n/g, "\n").split("\n");
+    });
+  console.log("Array1: ");
+  console.log(array1);
+  console.log("arr: ");
+  console.log("hi");
+}
+test1();
 export default function App() {
-  const [word, setWord] = useState("");
-  useEffect(() => {
-    fetch(
-      "https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt"
-    )
-      .then((words) => words.text())
-      .then((textedWords) => setWord(textedWords));
-  });
+  // const [word, setWord] = useState();
+  // useEffect(() => {
+  //   fetch(
+  //     "https://raw.githubusercontent.com/charlesreid1/five-letter-words/master/sgb-words.txt"
+  //   )
+  //     .then((words) => words.text())
+  //     .then((textedWords) => {
+  //       setWord(textedWords.split("\n"));
+  //     });
+  // });
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Wordle Answer</p>
       </header>
-      <body className="App-body">
+      <div className="App-body">
         <div>
           <input className="App-Input" type={"text"} maxLength={1} />
           <input className="App-Input" type={"text"} maxLength={1} />
@@ -25,8 +44,7 @@ export default function App() {
           <input className="App-Input" type={"text"} maxLength={1} />
         </div>
         <p>API calling</p>
-        <p>{word}</p>
-      </body>
+      </div>
     </div>
   );
 }
