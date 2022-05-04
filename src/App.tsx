08d4f3e2d01934435a8b353114d4a86f094f86d8
PlayerOne <playerprogramming@gmail.com>
2022-05-03 22:25:56 -0700

    Deleted Unncessary lines of code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 async function test1() {
   let arr;
@@ -35,10 +34,9 @@ export default function App() {
   }, []);
 
   const findAnswers = () => {
-    for(const eachWord of word){
-      
+    for (const eachWord of word) {
     }
-  }
+  };
 
   return (
     <div className="App">
@@ -94,23 +92,3 @@ export default function App() {
   );
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
