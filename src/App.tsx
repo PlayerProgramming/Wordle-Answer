4eab453ea057877a370fcf4eeb1a2b09471a9820
PlayerOne <playerprogramming@gmail.com>
2022-05-10 11:21:29 -0700

    Completed the most required functionality & UI

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,14 @@ async function wordArrays() {
       return arr;
     });
 }
-/*---------------------------  Render  -----------------------------*/
+/*---------------------------  Main  -----------------------------*/
 export default function App() {
   const [inputs, setInputs] = useState({
+    letter0: "",
     letter1: "",
     letter2: "",
     letter3: "",
     letter4: "",
-    letter5: "",
   });
 
   const [wordLists, setWordLists] = useState<string[]>([]);
@@ -29,75 +29,73 @@ export default function App() {
   const inputChange = (e: any) => {
     const { maxLength, value } = e.target;
     const id = e.target.getAttribute("data-id");
-    setInputs({
-      ...inputs,
-      [e.target.name]: e.target.value.toLowerCase(),
-    });
-    if (value.length >= maxLength) {
-      if (id < 4) {
-        const nextInput = document.querySelector(
-          "input[data-id='" + (parseInt(id) + 1) + "']"
-        );
-        console.log(nextInput);
-        if (nextInput !== null) {
-          (nextInput as HTMLElement).focus();
-          console.log("focused");
+    let nonContainedBreak = false;
+    for (const c of noncontainedLetters) {
+      if (c === value) {
+        nonContainedBreak = true;
+      }
+    }
+    if (!nonContainedBreak) {
+      setInputs({
+        ...inputs,
+        [e.target.name]: e.target.value.toLowerCase(),
+      });
+      if (value.length >= maxLength) {
+        if (id < 4) {
+          const nextInput = document.querySelector(
+            "input[data-id='" + (parseInt(id) + 1) + "']"
+          );
+          if (nextInput !== null) {
+            (nextInput as HTMLElement).focus();
+            console.log("focused");
+          }
         }
       }
     }
   };
   const containedChange = (e: any) => {
-    const { value } = e.target;
-    const lastChar = value.slice(-1);
-
+    const values = e.target.value;
+    const lastChar = values.slice(-1);
     const onlyLetters = e.target.value.replace(/[^a-zA-Z]/g, "");
 
+    /*Checking if lastChar is empty for deleting events */
     if (
-      e.target.name === "containedLetter" &&
-      !noncontainedLetters.includes(lastChar)
+      (e.target.name === "containedLetter" &&
+        !noncontainedLetters.includes(lastChar)) ||
+      (e.target.name === "containedLetter" && lastChar === "")
     ) {
       setContainedLetters(Array.from(new Set(onlyLetters.split(""))).join(""));
-    }
-    if (
-      e.target.name === "noncontainedLetter" &&
-      !containedLetters.includes(lastChar)
+    } else if (
+      (e.target.name === "noncontainedLetter" &&
+        !containedLetters.includes(lastChar)) ||
+      (e.target.name === "noncontainedLetter" && lastChar === "")
     ) {
       setNonContainedLetters(
         Array.from(new Set(onlyLetters.split(""))).join("")
       );
     }
   };
-
   const findAnswers = () => {
     let tempArray: string[] = [];
     for (const eachWord of wordLists) {
       let breakCheck1 = false;
       let breakCheck2 = false;
-      if (inputs.letter1 && inputs.letter1 !== eachWord[0]) {
-        continue;
-      }
-      if (inputs.letter2 && inputs.letter2 !== eachWord[1]) {
-        continue;
-      }
-      if (inputs.letter3 && inputs.letter3 !== eachWord[2]) {
-        continue;
-      }
-      if (inputs.letter4 && inputs.letter4 !== eachWord[3]) {
-        continue;
-      }
-      if (inputs.letter5 && inputs.letter5 !== eachWord[4]) {
+      if (
+        (inputs.letter0 && inputs.letter0 !== eachWord[0]) ||
+        (inputs.letter1 && inputs.letter1 !== eachWord[1]) ||
+        (inputs.letter2 && inputs.letter2 !== eachWord[2]) ||
+        (inputs.letter3 && inputs.letter3 !== eachWord[3]) ||
+        (inputs.letter4 && inputs.letter4 !== eachWord[4])
+      ) {
         continue;
       }
       for (const c of containedLetters) {
-        console.log(c);
         if (!eachWord.includes(c)) {
-          console.log("contained breaked");
           breakCheck1 = true;
         }
       }
       for (const c of noncontainedLetters) {
         if (eachWord.includes(c)) {
-          console.log("NON contained breaked");
           breakCheck2 = true;
         }
       }
@@ -109,14 +107,16 @@ export default function App() {
 
   const clearInput = () => {
     setInputs({
+      letter0: "",
       letter1: "",
       letter2: "",
       letter3: "",
       letter4: "",
-      letter5: "",
     });
     setContainedLetters("");
     setNonContainedLetters("");
+    const nextInput = document.querySelector("input[data-id='0']");
+    (nextInput as HTMLElement).focus();
   };
 
   useEffect(() => {
@@ -126,103 +126,114 @@ export default function App() {
         setWordLists(fetchedWord);
       });
     }
-    if (!Object.values(inputs).every((x) => x === null || x === "")) {
-      console.log("inputs detected");
+    if (
+      !Object.values(inputs).every((x) => x === null || x === "") ||
+      containedLetters !== "" ||
+      noncontainedLetters !== ""
+    ) {
       findAnswers();
     }
   }, [inputs, containedLetters, noncontainedLetters]);
 
+  /*---------------------------  Render  -----------------------------*/
   return (
     <div className="App">
       <header className="App-header">
         <p>Wordle Answer</p>
       </header>
       <div className="App-body">
-        <div className="App-Contained-Container">
-          <p>Contained Letters</p>
-          <input
-            className="App-Contain-Input"
-            name="containedLetter"
-            value={containedLetters}
-            autoComplete="off"
-            type={"text"}
-            onChange={containedChange}
-          />
-          <p>Non-contained letter</p>
-          <input
-            className="App-Contain-Input"
-            name="noncontainedLetter"
-            value={noncontainedLetters}
-            type={"text"}
-            onChange={containedChange}
-          />
+        <div className="App-body-Inner-Container">
+          <div className="App-Contained-Container">
+            <div>
+              <p>Contained Letters</p>
+              <input
+                className="App-Contain-Input"
+                name="containedLetter"
+                value={containedLetters}
+                autoComplete="off"
+                type={"text"}
+                onChange={containedChange}
+              />
+            </div>
+            <div>
+              <p>Non-contained letter</p>
+              <input
+                className="App-Contain-Input"
+                name="noncontainedLetter"
+                value={noncontainedLetters}
+                type={"text"}
+                onChange={containedChange}
+              />
+            </div>
+          </div>
+          <div className="App-Letter-Container">
+            <p>Letters</p>
+            <input
+              className="App-Input"
+              style={{ borderTopLeftRadius: 20, borderBottomLeftRadius: 20 }}
+              name="letter0"
+              data-id="0"
+              type={"text"}
+              value={inputs.letter0}
+              maxLength={1}
+              autoFocus={true}
+              onChange={inputChange}
+            />
+            <input
+              className="App-Input"
+              name="letter1"
+              data-id="1"
+              type={"text"}
+              value={inputs.letter1}
+              maxLength={1}
+              onChange={inputChange}
+            />
+            <input
+              className="App-Input"
+              name="letter2"
+              data-id="2"
+              type={"text"}
+              value={inputs.letter2}
+              maxLength={1}
+              onChange={inputChange}
+            />
+            <input
+              className="App-Input"
+              name="letter3"
+              data-id="3"
+              type={"text"}
+              value={inputs.letter3}
+              maxLength={1}
+              onChange={inputChange}
+            />
+            <input
+              className="App-Input"
+              style={{ borderTopRightRadius: 20, borderBottomRightRadius: 20 }}
+              name="letter4"
+              data-id="4"
+              type={"text"}
+              value={inputs.letter4}
+              maxLength={1}
+              onChange={inputChange}
+            />
+            <button className="App-Reset" onClick={clearInput}>
+              <p>Reset</p>
+            </button>
+          </div>
         </div>
-        <div>
-          <p>Letters</p>
-          <input
-            className="App-Input"
-            name="letter1"
-            data-id="0"
-            type={"text"}
-            value={inputs.letter1}
-            maxLength={1}
-            autoFocus={true}
-            onChange={inputChange}
-          />
-          <input
-            className="App-Input"
-            name="letter2"
-            data-id="1"
-            type={"text"}
-            value={inputs.letter2}
-            maxLength={1}
-            onChange={inputChange}
-          />
-          <input
-            className="App-Input"
-            name="letter3"
-            data-id="2"
-            type={"text"}
-            value={inputs.letter3}
-            maxLength={1}
-            onChange={inputChange}
-          />
-          <input
-            className="App-Input"
-            name="letter4"
-            data-id="3"
-            type={"text"}
-            value={inputs.letter4}
-            maxLength={1}
-            onChange={inputChange}
-          />
-          <input
-            className="App-Input"
-            name="letter5"
-            data-id="4"
-            type={"text"}
-            value={inputs.letter5}
-            maxLength={1}
-            onChange={inputChange}
-          />
-          <button style={{ width: 100, height: 100 }} onClick={clearInput}>
-            <p>Reset</p>
-          </button>
-        </div>
-
-        {(inputs.letter1 ||
+        {(inputs.letter0 ||
+          inputs.letter1 ||
           inputs.letter2 ||
           inputs.letter3 ||
           inputs.letter4 ||
-          inputs.letter5) && (
+          containedLetters ||
+          noncontainedLetters) && (
           <div className="App-Answers-Container">
-            {wordAnswers?.map((word, i) => {
-              return (
-                <p className="App-Answers-Items" key={i}>
-                  {word}
-                </p>
-              );
-            })}
+            {wordAnswers?.map((word, i) => (
+              <div key={i} className="App-Answers-Items-Container">
+                <p className="App-Answers-Items">{word}</p>
+              </div>
+            ))}
           </div>
         )}
       </div>
